4a01b298a32e86aa2d7d6068ae07747474c8c4fc
FireRun <entryfirerun7012@outlook.com>
2024-10-26 21:37:27 +0900

    Add files via upload

--- hello-world.js
+++ hello-world.js
@@ -3,9 +3,9 @@ function createLinkButton(realThis, realJ) {
     const linkButton = document.createElement("a");
     linkButton.className = "videoLink NoFind";
     linkButton.innerText = "바로가기";
-    linkButton.href = `https://firebasestorage.googleapis.com/v0/b/imgshare-2.appspot.com/o/${realJ}?alt=media`;
+    linkButton.href = `https://bloupla.net/img/?=${realJ}`;
     realThis.before(linkButton);
-    realThis.outerHTML = `<video controls style='outline: 1px solid red;' class='realImage' src='https://bloupla.net/img/?=${realJ}'></video>`;
+    realThis.outerHTML = `<video controls style='outline: 1px solid red;' class='realImage' src='https://firebasestorage.googleapis.com/v0/b/imgshare-2.appspot.com/o/${realJ}?alt=media'></video>`;
 }
 
 function createLinkButtonEntry(realThis, realJ) {
